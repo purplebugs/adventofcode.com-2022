3eaa37ab9043dab1b62ed761eb534849de5d0141
Anita Lipsky <anita@purplebugs.com>
2022-12-03 10:27:02 +0100

    Day 1 solved

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-// TODO get input
-// TODO process input and return result
+import { readFileSync } from "fs";
 
-console.log("TODO day 1");
+const day_1 = "day-1.txt"; //  https://adventofcode.com/2022/day/1
+
+// Read file from disk
+const data = readFileSync(`./data/${day_1}`, "utf8");
+
+const arrayOfText = data.split("\n");
+
+let caloriesPerElf = [];
+let totalCaloriesOfCurrentElf = 0;
+arrayOfText.forEach((item) => {
+  if (item !== "") {
+    totalCaloriesOfCurrentElf = parseInt(item) + totalCaloriesOfCurrentElf;
+  }
+  if (item === "") {
+    caloriesPerElf.push(totalCaloriesOfCurrentElf);
+    totalCaloriesOfCurrentElf = 0;
+  }
+});
+
+console.log(`List of calories per elf: ${caloriesPerElf}`);
+console.log(`Number of elves: ${caloriesPerElf.length}`);
+console.log(
+  `Total calories of elf with the most calories: ${Math.max(...caloriesPerElf)}`
+);
