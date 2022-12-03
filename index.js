e0358b6e74d801c814d61850ab18098a6d61446d
Anita Lipsky <anita@purplebugs.com>
2022-12-03 16:21:54 +0100

    Solution Day one part two

--- index.js
+++ index.js
@@ -7,20 +7,47 @@ const data = readFileSync(`./data/${day_1}`, "utf8");
 
 const arrayOfText = data.split("\n");
 
-let caloriesPerElf = [];
-let totalCaloriesOfCurrentElf = 0;
-arrayOfText.forEach((item) => {
-  if (item !== "") {
-    totalCaloriesOfCurrentElf = parseInt(item) + totalCaloriesOfCurrentElf;
-  }
-  if (item === "") {
-    caloriesPerElf.push(totalCaloriesOfCurrentElf);
-    totalCaloriesOfCurrentElf = 0;
-  }
-});
+const generateCaloriesPerElfArray = (arrayOfText) => {
+  const result = [];
+  let totalCaloriesOfCurrentElf = 0;
+  arrayOfText.forEach((item) => {
+    if (item !== "") {
+      totalCaloriesOfCurrentElf = parseInt(item) + totalCaloriesOfCurrentElf;
+    }
+    if (item === "") {
+      result.push(totalCaloriesOfCurrentElf);
+      totalCaloriesOfCurrentElf = 0;
+    }
+  });
+  return result;
+};
 
-console.log(`List of calories per elf: ${caloriesPerElf}`);
-console.log(`Number of elves: ${caloriesPerElf.length}`);
+const compareNumbers = (a, b) => {
+  return b - a;
+};
+
+const sortCaloriesPerElfArray = (numberArray) => {
+  return numberArray.sort(compareNumbers);
+};
+
+console.log("DAY ONE - Part One");
+
+const caloriesPerElfArray = generateCaloriesPerElfArray(arrayOfText);
 console.log(
-  `Total calories of elf with the most calories: ${Math.max(...caloriesPerElf)}`
+  `Total calories of elf with the most calories: ${Math.max(
+    ...caloriesPerElfArray
+  )}\n`
+);
+
+console.log("DAY ONE - Part Two");
+const caloriesPerElfArraySorted = sortCaloriesPerElfArray(caloriesPerElfArray);
+const sumTopThreeCaloriesPerElf = (numberArray) => {
+  let result = 0;
+  result = numberArray[0] + numberArray[1] + numberArray[2];
+  return result;
+};
+console.log(
+  `Total calories of top three elves with the most calories: ${sumTopThreeCaloriesPerElf(
+    caloriesPerElfArraySorted
+  )}`
 );
