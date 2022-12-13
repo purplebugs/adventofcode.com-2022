b3a0cd5db3dede0a7ed5e8dbe772dfaf87dfab42
Anita Lipsky <anita@purplebugs.com>
2022-12-13 22:25:10 +0100

    Day 5 Part 2

--- functions/day-5.js
+++ functions/day-5.js
@@ -73,26 +73,92 @@ export const day_5 = () => {
       };
     });
 
-  const moveCargo = (move, from, to) => {
-    const temp = cargo[from].slice(-move).reverse();
+  const moveCargo = (move, from, to, isPartOne) => {
+    let temp = null;
+    if (isPartOne) {
+      temp = cargo[from].slice(-move).reverse();
+    }
+    if (!isPartOne) {
+      temp = cargo[from].slice(-move);
+    }
     cargo[from].splice(-move, move);
     cargo[to].push(...temp);
   };
 
-  instructions.forEach((instruction, i) => {
-    moveCargo(instruction.move, instruction.from, instruction.to);
+  let topsPartOne = [];
+  instructions.forEach((instruction, isPartOne = true) => {
+    moveCargo(instruction.move, instruction.from, instruction.to, isPartOne);
   });
 
-  // console.log(`${JSON.stringify(cargo)}\n`);
-
-  let tops = [];
-
   for (let i = 1; i <= 9; i++) {
-    tops.push(cargo[i][cargo[i].length - 1]);
+    topsPartOne.push(cargo[i][cargo[i].length - 1]);
   }
 
+  cargo = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: [],
+    8: [],
+    9: [],
+  };
+  crates.forEach((line) => {
+    let rows = [];
+    const one = line.substring(1, 2);
+    const two = line.substring(5, 6);
+    const three = line.substring(9, 10);
+    const four = line.substring(13, 14);
+    const five = line.substring(17, 18);
+    const six = line.substring(21, 22);
+    const seven = line.substring(25, 26);
+    const eight = line.substring(29, 30);
+    const nine = line.substring(33, 34);
+    rows.push(one, two, three, four, five, six, seven, eight, nine);
+
+    if (one !== " ") {
+      cargo[1].unshift(one);
+    }
+    if (two !== " ") {
+      cargo[2].unshift(two);
+    }
+    if (three !== " ") {
+      cargo[3].unshift(three);
+    }
+    if (four !== " ") {
+      cargo[4].unshift(four);
+    }
+    if (five !== " ") {
+      cargo[5].unshift(five);
+    }
+    if (six !== " ") {
+      cargo[6].unshift(six);
+    }
+    if (seven !== " ") {
+      cargo[7].unshift(seven);
+    }
+    if (eight !== " ") {
+      cargo[8].unshift(eight);
+    }
+    if (nine !== " ") {
+      cargo[9].unshift(nine);
+    }
+  });
+
+  let topsPartTwo = [];
+  instructions.forEach((instruction) => {
+    moveCargo(instruction.move, instruction.from, instruction.to, false);
+  });
+
+  for (let i = 1; i <= 9; i++) {
+    topsPartTwo.push(cargo[i][cargo[i].length - 1]);
+  }
+  //console.log(`${JSON.stringify(cargo)}\n`);
+
   console.log("DAY FIVE - Part One");
-  console.log(`Crates on top of each stack: ${tops.join("")}\n`);
+  console.log(`Crates on top of each stack: ${topsPartOne.join("")}\n`);
   console.log("DAY FIVE - Part Two");
-  console.log(`TODO\n\n`);
+  console.log(`Crates on top of each stack: ${topsPartTwo.join("")}\n\n`);
 };
