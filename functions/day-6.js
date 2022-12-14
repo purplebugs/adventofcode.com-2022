a0c63c8cd3305e4712879446707e0f87014b86aa
Anita Lipsky <anita@purplebugs.com>
2022-12-14 19:46:02 +0100

    Day Six Part Two

--- functions/day-6.js
+++ functions/day-6.js
@@ -7,28 +7,33 @@ export const day_6 = () => {
   const data = readFileSync(`./data/${day_6}`, "utf8");
   const signals = data.split("");
 
-  const span = 4;
   let charsProcessed = 0;
 
-  for (let i = 0; i < signals.length - (span - 1); i++) {
-    // Create array of distinct chars
-    const chars = signals.slice(i, i + span);
-    const distinctChars = [...new Set(chars)];
+  const findAdjoiningUniqueCharsEndPosition = (signals, span) => {
+    for (let i = 0; i < signals.length - (span - 1); i++) {
+      // Create array of distinct chars
+      const chars = signals.slice(i, i + span);
+      const distinctChars = [...new Set(chars)];
 
-    // console.log(chars);
-    // console.log(distinctChars);
-    // console.log(
-    //   `${i}: ${signals[i]}${signals[i + 1]}${signals[i + 2]}${signals[i + 3]}\n`
-    // );
+      // console.log(chars);
+      // console.log(distinctChars);
+      // console.log(
+      //   `${i}: ${signals[i]}${signals[i + 1]}${signals[i + 2]}${signals[i + 3]}\n`
+      // );
 
-    if (distinctChars.length === span) {
-      charsProcessed = i + span;
-      // Found unique set of adjacent chars
-      break;
+      if (distinctChars.length === span) {
+        // Found unique set of adjacent chars
+        return (charsProcessed = i + span);
+      }
     }
-  }
+  };
+
   console.log("DAY SIX - Part One");
-  console.log(`Chars processed: ${charsProcessed}\n`);
+  console.log(
+    `Chars processed: ${findAdjoiningUniqueCharsEndPosition(signals, 4)}\n`
+  );
   console.log("DAY SIX - Part Two");
-  console.log(`TODO \n\n`);
+  console.log(
+    `Chars processed: ${findAdjoiningUniqueCharsEndPosition(signals, 14)}\n\n`
+  );
 };
