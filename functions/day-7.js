0ae6a7499583991d98313f2dfd8b993e4bd3e65f
Anita Lipsky <anita@purplebugs.com>
2022-12-15 14:29:04 +0100

    clean

--- functions/day-7.js
+++ functions/day-7.js
@@ -3,31 +3,6 @@ import { readFileSync } from "fs";
 export const day_7 = () => {
   const day_7 = "day-7.txt"; //  https://adventofcode.com/2022/day/7
 
-  // Read file from disk
-  //   const data = `$ cd /
-  // $ ls
-  // dir a
-  // 14848514 b.txt
-  // 8504156 c.dat
-  // dir d
-  // $ cd a
-  // $ ls
-  // dir e
-  // 29116 f
-  // 2557 g
-  // 62596 h.lst
-  // $ cd e
-  // $ ls
-  // 584 i
-  // $ cd ..
-  // $ cd ..
-  // $ cd d
-  // $ ls
-  // 4060174 j
-  // 8033020 d.log
-  // 5626152 d.ext
-  // 7214296 k`;
-
   const data = readFileSync(`./data/${day_7}`, "utf8");
   const signals = data.split("\n"); //
 
