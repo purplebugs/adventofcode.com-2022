cf4549eba13556e0811a9425bbfe7edbf6f659d1
Anita Lipsky <anita@purplebugs.com>
2022-12-15 21:26:13 +0100

    Day 7 work in progress

--- functions/day-7.js
+++ functions/day-7.js
@@ -4,56 +4,64 @@ export const day_7 = () => {
   const day_7 = "day-7.txt"; //  https://adventofcode.com/2022/day/7
 
   const data = readFileSync(`./data/${day_7}`, "utf8");
-  const signals = data.split("\n"); //
+  const commands = data.split("\n");
 
-  const commands = signals.map((signal, i) => {
-    if (signal.startsWith("$ cd ")) {
+  let currentLevel = 0;
+  let currentParent = 0;
+
+  const entries = commands.map((command, i) => {
+    if (command.startsWith("$ cd ")) {
+      const value = command.substring(5);
+      if (value === "/") {
+        currentLevel = 0;
+      }
+      if (value === "..") {
+        currentLevel -= 1;
+      }
+      if (value !== "/" && value !== "..") {
+        currentLevel += 1;
+      }
       return {
         id: i,
         command: "cd",
-        value: signal.substring(5),
+        value: value,
+        level: currentLevel, // TODO
+        parent: currentParent, // TODO
       };
     }
-    if (signal.startsWith("dir ")) {
+    if (command.startsWith("dir ")) {
       return {
         id: i,
         command: "dir",
-        value: signal.substring(4),
+        value: command.substring(4),
+        level: currentLevel, // TODO
+        parent: currentParent, // TODO
       };
     }
-    if (signal.startsWith("$ ls")) {
+    if (command.startsWith("$ ls")) {
       return {
         id: i,
         command: "ls",
+        level: currentLevel, // TODO
+        parent: currentParent, // TODO
       };
     }
     const regex = RegExp(`([0-9])*`, "g");
-    const fileSize = signal.match(regex);
-    if (signal.match(regex)) {
+    const fileSize = command.match(regex);
+    if (command.match(regex)) {
       return {
         id: i,
         command: "file",
         size: fileSize[0],
-        value: signal.substring(fileSize[0].length + 1),
+        value: command.substring(fileSize[0].length + 1),
+        level: currentLevel, // TODO
+        parent: currentParent, // TODO
       };
     }
   });
 
-  // Ref: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-  const names = ["Alice", "Bob", "Tiff", "Bruce", "Alice"];
-  const countedNames = names.reduce((allNames, name) => {
-    const currCount = allNames[name] ?? 0;
-
-    return {
-      ...allNames,
-      [name]: currCount + 1,
-    };
-  }, {});
-  // countedNames is:
-  // { 'Alice': 2, 'Bob': 1, 'Tiff': 1, 'Bruce': 1 }
-
   console.log("DAY SEVEN - Part One");
-  //console.log(`TODO: ${JSON.stringify(commands)}\n`);
+  console.log(`TODO: ${JSON.stringify(entries)}\n`);
   console.log("DAY SEVEN - Part Two");
   console.log(`TODO: \n\n`);
 };
