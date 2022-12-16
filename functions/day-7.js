5f3fc1633cc5c3291fa4286c5aca55227febd6ac
Anita Lipsky <anita@purplebugs.com>
2022-12-16 10:30:32 +0100

    Day 7 Part 1 - Build flat entries: levels,parents

--- functions/day-7.js
+++ functions/day-7.js
@@ -6,49 +6,60 @@ export const day_7 = () => {
   const data = readFileSync(`./data/${day_7}`, "utf8");
   const commands = data.split("\n");
 
-  let currentLevel = 0;
-  let currentParent = 0;
-  const parents = [];
+  let level = 0;
+  let parent = 0;
+  const parents = new Map();
+
   const entries = commands.map((command, i) => {
     if (command.startsWith("$ cd ")) {
       const value = command.substring(5);
+      let obj = { id: i, command: "cd", value: value };
       if (value === "/") {
-        currentLevel = 0;
-        currentParent = null;
-      }
-      if (value === "..") {
-        currentLevel -= 1;
-        currentParent = parents[currentLevel].id;
+        obj = {
+          ...obj,
+          level: 0,
+          parent: null,
+        };
+        if (!parents.get(0)) {
+          parents.set(0, i);
+        }
       }
       if (value !== "/" && value !== "..") {
-        currentLevel += 1;
-        currentParent = parents[currentLevel - 1].id;
+        level += 1;
+        obj = {
+          ...obj,
+          level: level,
+          parent: parents.get(level - 1),
+        };
       }
-      const obj = {
-        id: i,
-        command: "cd",
-        value: value,
-        level: currentLevel,
-        parent: currentParent, // TODO
-      };
-      parents.push(obj);
+      if (value === "..") {
+        level -= 1;
+        obj = {
+          ...obj,
+          level: level,
+          parent: parents.get(level - 2),
+        };
+      }
+
       return obj;
     }
     if (command.startsWith("dir ")) {
+      if (!parents.get(level)) {
+        parents.set(level, i);
+      }
       return {
         id: i,
         command: "dir",
         value: command.substring(4),
-        level: currentLevel,
-        parent: currentParent, // TODO
+        level: level,
+        parent: parents.get(level - 1),
       };
     }
     if (command.startsWith("$ ls")) {
       return {
         id: i,
         command: "ls",
-        level: currentLevel,
-        parent: currentParent, // TODO
+        level: level,
       };
     }
     const regex = RegExp(`([0-9])*`, "g");
@@ -59,15 +70,22 @@ export const day_7 = () => {
         command: "file",
         size: fileSize[0],
         value: command.substring(fileSize[0].length + 1),
-        level: currentLevel,
-        parent: currentParent, // TODO
+        level: level,
+        parent: parents.get(level - 1),
       };
     }
   });
 
+  // function logMapElements(value, key) {
+  //   console.log(`parents[${key}] = ${value}`);
+  // }
+
+  // parents.forEach(logMapElements);
+
   console.log("DAY SEVEN - Part One");
-  // console.log(`TODO: ${JSON.stringify(parents)}\n`);
-  // console.log(`TODO: ${JSON.stringify(entries)}\n`);
+  // console.log(`TODO:  parents ${JSON.stringify(parents)}\n`);
+  //console.log(`-------------`);
+  //console.log(`TODO: ${JSON.stringify(entries)}\n`);
   console.log("DAY SEVEN - Part Two");
   console.log(`TODO: \n\n`);
 };
