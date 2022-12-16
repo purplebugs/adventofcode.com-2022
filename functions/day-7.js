d9f0f1b44e2f06ed52fae8d388582632c49a45b1
Anita Lipsky <anita@purplebugs.com>
2022-12-16 14:01:12 +0100

    Day 7 - remove ls commands

--- functions/day-7.js
+++ functions/day-7.js
@@ -9,79 +9,78 @@ export const day_7 = () => {
   let level = 0;
   const parents = new Map();
 
-  const entries = commands.map((command, i) => {
-    if (command.startsWith("$ cd ")) {
-      const value = command.substring(5);
-      let obj = { id: i, command: "cd", value: value };
-      if (value === "/") {
-        obj = {
-          ...obj,
-          level: 0,
-        };
-        const rootParent = parents.get(`0:${value}`);
-        if (rootParent === undefined) {
-          parents.set(`0:${value}`, i);
+  const entries = commands
+    .map((command, i) => {
+      if (command.startsWith("$ cd ")) {
+        const value = command.substring(5);
+        let obj = { id: i, command: "cd", value: value };
+        if (value === "/") {
+          obj = {
+            ...obj,
+            level: 0,
+          };
+          const rootParent = parents.get(`0:${value}`);
+          if (rootParent === undefined) {
+            parents.set(`0:${value}`, i);
+          }
         }
+
+        if (value !== "/" && value !== "..") {
+          level += 1;
+          obj = {
+            ...obj,
+            level: level,
+          };
+        }
+        if (value === "..") {
+          level -= 1;
+          obj = {
+            ...obj,
+            level: level,
+          };
+        }
+
+        return obj;
+      }
+      if (command.startsWith("dir ")) {
+        const value = command.substring(4);
+        parents.set(`${level}:${value}`, i);
+        return {
+          id: i,
+          command: "dir",
+          value: value,
+          level: level,
+          children: [],
+        };
       }
 
-      if (value !== "/" && value !== "..") {
-        level += 1;
-        obj = {
-          ...obj,
+      if (command.startsWith("$ ls")) {
+        return null;
+      }
+      const regex = RegExp(`([0-9])*`, "g");
+      const fileSize = command.match(regex);
+      if (command.match(regex)) {
+        return {
+          id: i,
+          command: "file",
+          size: fileSize[0],
+          value: command.substring(fileSize[0].length + 1),
           level: level,
         };
       }
-      if (value === "..") {
-        level -= 1;
-        obj = {
-          ...obj,
-          level: level,
-        };
-      }
+    })
+    .filter((entry) => entry !== null);
 
-      return obj;
-    }
-    if (command.startsWith("dir ")) {
-      const value = command.substring(4);
-      parents.set(`${level}:${value}`, i);
-      return {
-        id: i,
-        command: "dir",
-        value: value,
-        level: level,
-        children: [],
-      };
-    }
-    if (command.startsWith("$ ls")) {
-      return {
-        id: i,
-        command: "ls",
-        level: level,
-      };
-    }
-    const regex = RegExp(`([0-9])*`, "g");
-    const fileSize = command.match(regex);
-    if (command.match(regex)) {
-      return {
-        id: i,
-        command: "file",
-        size: fileSize[0],
-        value: command.substring(fileSize[0].length + 1),
-        level: level,
-      };
-    }
-  });
-
+  console.log("DAY SEVEN - Part One");
   function logMapElements(value, key) {
     console.log(`parents[${key}] = ${value}`);
   }
 
-  //parents.forEach(logMapElements);
+  parents.forEach(logMapElements);
 
   // TODO Build tree with recursion
 
-  console.log("DAY SEVEN - Part One");
-  //console.log(`TODO: ${JSON.stringify(entries)}\n`);
+  console.log(`TODO: ${JSON.stringify(entries)}\n`);
   console.log("DAY SEVEN - Part Two");
   console.log(`TODO: \n\n`);
 };
