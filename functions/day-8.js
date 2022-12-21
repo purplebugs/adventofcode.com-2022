27e0d53adcb1418975bf656466d24e1d767904da
Anita Lipsky <anita@purplebugs.com>
2022-12-22 00:02:48 +0100

    Day 8 full data import

--- functions/day-8.js
+++ functions/day-8.js
@@ -2,6 +2,7 @@ import { readFileSync } from "fs";
 
 export const day_8 = () => {
   const day_8 = "day-8-example.txt"; //  https://adventofcode.com/2022/day/8
+  //const day_8 = "day-8.txt"; //  https://adventofcode.com/2022/day/8
 
   const data = readFileSync(`./data/${day_8}`, "utf8");
   const numberLines = data.split("\n");
