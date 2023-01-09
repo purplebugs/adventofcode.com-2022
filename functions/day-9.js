a740fb0309bee45b2fd506c8217e0458208035bc
Anita Lipsky <anita@purplebugs.com>
2023-01-09 16:18:08 +0100

    Still buggy, main elements done

--- functions/day-9.js
+++ functions/day-9.js
@@ -35,50 +35,69 @@ class Matrix {
     if (Math.abs(head.x - tail.x) > -1) {
       isTouching = true;
     }
-    // TODO add logic for one space apart in any direction
+    if (Math.abs(head.y - tail.y) > -1) {
+      isTouching = true;
+    }
     return isTouching;
   }
 
   create(data = "") {
     const rawInstructions = data.split("\n");
-    let instructions = rawInstructions.map((instruction) => {
+    let instructions = rawInstructions.map((instructionLine) => {
       // format eg ["R 4", "L 2"] as [R,R,R,R,L,L]
 
-      let instructionsWithRepeater = [];
-      const repeater = +instruction[2];
+      let instruction = [];
+      const repeater = +instructionLine[2];
       for (let i = 1; i <= repeater; i++) {
-        instructionsWithRepeater.push(instruction[0]);
+        instruction.push(instructionLine[0]);
       }
-      return instructionsWithRepeater;
+      return instruction;
     });
 
     instructions = instructions.flatMap((instruction) => instruction);
-    console.log(instructions);
-
-    let head = this.currentHead;
-    let tail = this.currentTail;
 
     this.matrix = instructions.forEach((line) => {
       const toInstructionLine = line.split(" ");
 
-      if (this.isTouching(head, tail)) {
-        // console.log("is touching");
-        // console.log("this.isTouching(head, tail)", this.isTouching(head, tail));
+      if (toInstructionLine[0] === "R") {
+        this.width = this.width + 1;
+        this.currentHead = new Point(
+          this.currentHead.x + 1,
+          this.currentHead.y,
+          "H"
+        );
+      }
+      if (toInstructionLine[0] === "L") {
+        this.width = this.width - 1;
+        this.currentHead = new Point(
+          this.currentHead.x - 1,
+          this.currentHead.y,
+          "H"
+        );
+      }
+      if (toInstructionLine[0] === "U") {
+        this.height = this.height + 1;
+        this.currentHead = new Point(
+          this.currentHead.y + 1,
+          this.currentHead.y,
+          "H"
+        );
+      }
+      if (toInstructionLine[0] === "D") {
+        this.height = this.height - 1;
+        this.currentHead = new Point(
+          this.currentHead.y - 1,
+          this.currentHead.y,
+          "H"
+        );
+      }
+
+      if (this.isTouching(this.currentHead, this.currentTail)) {
         this.currentTail = this.currentHead;
         this.visitedTailPositions.push({
           x: this.currentTail.x,
           y: this.currentTail.y,
         });
-      }
-
-      // TODO handle L, U, D
-      if (toInstructionLine[0] === "R") {
-        this.width = this.width + 1;
-        this.currentHead = new Point(this.currentHead.x + 1, head.y, "H");
-      }
-      if (toInstructionLine[0] === "L") {
-        this.width = this.width - 1;
-        this.currentHead = new Point(this.currentHead.x - 1, head.y, "H");
       }
     });
   }
